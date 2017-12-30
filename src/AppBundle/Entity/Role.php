1b2747450f9101ce9c6c2522b9da5383b5d8809c
Krasimira Georgieva <krassi77bg@gmail.com>
2017-12-30 12:01:03 +0200

    Removed unused statement.

--- src/AppBundle/Entity/Role.php
+++ src/AppBundle/Entity/Role.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
-use Symfony\Component\Security\Core\Role\RoleInterface;
+
 
 /**
  * Role
